d64f978cbc8fec34e0f5b99c8751e83a867dee6b
Alex Vazquez <avazque008@outlook.com>
2022-05-01 20:01:38 -0400

    new folders, files and functions to write and create the README file

--- index.js
+++ index.js
@@ -1,13 +1,162 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateFileTemplate = require('./src/write-readme-template');
+const writeFile = require('./utils/create-readme-file');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a short description about your project:',
+            validate: projectDescription => {
+              if (projectDescription) {
+                return true;
+              } else {
+                console.log('Please provide a description of the project!');
+                return false;
+              }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installationInstructions',
+            message: 'What are the steps required to install your project?',
+            validate: installationInstructions => {
+                if (installationInstructions) {
+                    return true;
+                } else {
+                    console.log('Please provide a step-by-step description of how to get the development environment running!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usageInformation',
+            message: 'Please provide instructions and examples for use of your project:',
+            validate: usageInformation => {
+                if (usageInformation) {
+                    return true;
+                } else {
+                    console.log('Please provide a step-by-step description of how to use your appliction!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose one of the following open source licenses:',
+            choices: ['Apache License 2.0', 'MIT License', 'GNU General Public License v3.0'],
+        },
+        {
+            type: 'confirm',
+            name: 'ccg',
+            message: 'Would you like to adapt Contributor Covenant as your contribution guidelines for your project?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'contributionGuidelines',
+            message: 'Provide your contribution guidelines for your project:',
+            when: ({ CCG }) => {
+                if (!CCG) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'testsInstructions',
+            message: 'Please provide tests instructions for your application:',
+            validate: testsInstructions => {
+                if (testsInstructions) {
+                    return true;
+                } else {
+                    console.log('Please provide a step-by-step description of how to run the tests for your application!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub Username:',
+            validate: githubUsername => {
+                if (githubUsername) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email:',
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('Please enter your email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Please provide instructions on how to reach you with additional questions:',
+            validate: questions => {
+                if (questions) {
+                    return true;
+                } else {
+                    console.log('Please provide a step-by-step description of how to reach you with additional questions!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'firstLastName',
+            message: 'Please enter your first and last name:',
+            validate: firstLastName => {
+                if (firstLastName) {
+                    return true;
+                } else {
+                    console.log('Please enter your first and last name!');
+                    return false;
+                }
+            }
+        }
+                               
+    ]);
+};
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+promptUser()
+    .then(userData => {
+        return generateFileTemplate(userData);
+    })
+    .then(fileTemplate => {
+        return writeFile(fileTemplate);
+    })
+    .catch(err => {
+        console.log(err);
+    });
